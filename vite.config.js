63c2ce1166df2c0ffa77a68271f1a8e45123bdd6
SangJin983 <lazistar983@gmail.com>
2025-03-13 23:27:29 +0900

    refactor: 더 깔끔한 임포트를 위한 경로 별칭 추가

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
@@ -6,5 +7,10 @@ export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
 });
