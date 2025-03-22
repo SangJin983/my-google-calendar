4a689c1318c1ecb7731833745f965a59c069c98a
SangJin983 <lazistar983@gmail.com>
2025-03-23 02:20:47 +0900

    feat: event crud 액션 추가 - eventApi를 이용해 fetchEvents 리팩토링 - eventApi를 이용해 createEvent, updateEvent, deleteEvent 비동기 액션 구현 - 비동기 액션을 처리하기 위한 setStatus헬퍼함수 분리 작성

--- src/features/events/slices/eventSlice.js
+++ src/features/events/slices/eventSlice.js
@@ -1,21 +1,56 @@
+import { STATUS } from "@common/constants";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { supabase } from "@services/supabaseClient";
+import {
+  createEventApi,
+  deleteEventApi,
+  fetchEventsApi,
+  updateEventApi,
+} from "../api/eventApi";
 
 export const fetchEvents = createAsyncThunk("events/fetchEvents", async () => {
-  const { data, error } = await supabase.from("events").select("*");
-
-  if (error) {
-    throw error;
-  }
-
+  const data = await fetchEventsApi();
   return data;
 });
+export const createEvent = createAsyncThunk(
+  "events/createEvent",
+  async (eventData) => {
+    const data = await createEventApi(eventData);
+    return data;
+  }
+);
+export const updateEvent = createAsyncThunk(
+  "events/updateEvent",
+  async ({ id, updates }) => {
+    const data = await updateEventApi({ id, updates });
+    return data;
+  }
+);
+export const deleteEvent = createAsyncThunk(
+  "events/deleteEvent",
+  async (id) => {
+    await deleteEventApi(id);
+    return id;
+  }
+);
 
 const initialState = {
   ids: [],
   entities: {},
-  status: "idle",
+  status: STATUS.IDLE,
   error: null,
+};
+
+const setPendingStatus = (state) => {
+  state.status = STATUS.LOADING;
+  state.error = null; // 에러 메세지 초기화
+};
+const setSuccessStatus = (state) => {
+  state.status = STATUS.SUCCEEDED;
+  state.error = null;
+};
+const setFailedStatus = (state, action) => {
+  state.status = STATUS.FAILED;
+  state.error = action.error.message;
 };
 
 const eventSlice = createSlice({
@@ -24,11 +59,10 @@ const eventSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchEvents.pending, (state) => {
-        state.status = "loading";
-      })
+      // fetchEvents 액션 처리
+      .addCase(fetchEvents.pending, setPendingStatus)
       .addCase(fetchEvents.fulfilled, (state, action) => {
-        state.status = "succeeded";
+        setSuccessStatus(state);
         // 데이터를 정규화 상태에 맞게 초기화
         state.ids = [];
         state.entities = {};
@@ -38,10 +72,33 @@ const eventSlice = createSlice({
           state.entities[event.id] = event;
         });
       })
-      .addCase(fetchEvents.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      });
+      .addCase(fetchEvents.rejected, setFailedStatus)
+      // createEvent 액션 처리
+      .addCase(createEvent.pending, setPendingStatus)
+      .addCase(createEvent.fulfilled, (state, action) => {
+        setSuccessStatus(state);
+        const newEvent = action.payload[0];
+        state.ids.push(newEvent.id);
+        state.entities[newEvent.id] = newEvent;
+      })
+      .addCase(createEvent.rejected, setFailedStatus)
+      // updateEvent 액션 처리
+      .addCase(updateEvent.pending, setPendingStatus)
+      .addCase(updateEvent.fulfilled, (state, action) => {
+        setSuccessStatus(state);
+        const updatedEvent = action.payload[0];
+        state.entities[updatedEvent.id] = updatedEvent;
+      })
+      .addCase(updateEvent.rejected, setFailedStatus)
+      // deleteEvent 액션 처리
+      .addCase(deleteEvent.pending, setPendingStatus)
+      .addCase(deleteEvent.fulfilled, (state, action) => {
+        setSuccessStatus(state);
+        const deletedId = action.payload;
+        state.ids.filter((id) => id !== deletedId);
+        delete state.entities[deletedId];
+      })
+      .addCase(deleteEvent.rejected, setFailedStatus);
   },
 });
 
