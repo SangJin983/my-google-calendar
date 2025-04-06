e7ba96a63f1e8fce69877b7b0208f37e7c5cf49b
SangJin983 <lazistar983@gmail.com>
2025-04-07 03:35:43 +0900

    feat: ErrorMessage 컴포넌트 추가

---
+++ src/common/components/ErrorMessage.jsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+const ErrorWrapper = styled.div`
+  padding: 10px 15px;
+  margin-top: 15px;
+  border: 1px solid #dc3545;
+  border-radius: 4px;
+  background-color: #f8d7da;
+  color: #721c24;
+  font-size: 0.9rem;
+  display: flex;
+  align-items: center;
+`;
+
+const ErrorMessage = ({ message }) => {
+  if (!message) {
+    return null;
+  }
+
+  return <ErrorWrapper>{message}</ErrorWrapper>;
+};
+
+export default ErrorMessage;
