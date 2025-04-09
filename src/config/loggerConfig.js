85e4e63617099211535eea0cfdf7b1829f8fe227
SangJin983 <lazistar983@gmail.com>
2025-04-09 21:35:15 +0900

    refactor: loggerConfig에 HANDLER 추가

--- src/config/loggerConfig.js
+++ src/config/loggerConfig.js
@@ -1,4 +1,5 @@
 import { LOG_LEVELS } from "@common/constants";
+import { consoleLogHandler } from "@common/utils";
 
 const logLevelName =
   import.meta.env.VITE_LOG_LEVEL || (import.meta.env.PROD ? "INFO" : "DEBUG");
@@ -6,7 +7,10 @@ const logLevelName =
 const currentLogLevel =
   LOG_LEVELS[logLevelName.toUpperCase()] ?? LOG_LEVELS.DEBUG;
 
+const currentLogHandler = consoleLogHandler;
+
 export const LOGGER_CONFIG = {
   LEVEL: currentLogLevel,
   LEVEL_NAME: logLevelName,
+  HANDLER: currentLogHandler,
 };
